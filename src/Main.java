c9dc4359ac960a5bffa83ca9e0e9a631b41cd7b7
Lertos <deetasticgaming@gmail.com>
2022-10-21 11:05:36 -0300

    Setting up the Scanner loop

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String response = "";
+
+        System.out.println("Please enter a sentence and press Enter. The vowels will be counted.");
+
+        while (!response.equalsIgnoreCase("q")) {
+            response = scanner.nextLine();
+
+            //Ignore empty strings
+            if (response.isEmpty()) {
+                continue;
+            }
+
+            //Debugging - to see their response
+            System.out.println(response);
+        }
     }
 }
