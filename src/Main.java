f8a64493fd31aefa209a4c1502269114812206ef
Lertos <deetasticgaming@gmail.com>
2022-10-21 11:42:38 -0300

    Creating the naive approach to counting the vowels

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Main {
+
+    private static final char[] vowels = {'a', 'e', 'i', 'o', 'u', 'y'};
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String response = "";
@@ -10,13 +13,29 @@ public class Main {
         while (!response.equalsIgnoreCase("q")) {
             response = scanner.nextLine();
 
-            //Ignore empty strings
-            if (response.isEmpty()) {
-                continue;
+            if (response.equalsIgnoreCase("q")) {
+                break;
             }
 
-            //Debugging - to see their response
-            System.out.println(response);
+            int count = naiveApproach(response);
+
+            System.out.println("Your text had " + count + " vowels in it.");
+            System.out.println("\nSupply another phrase, or type 'q' to quit.");
         }
+    }
+
+    private static int naiveApproach(String text) {
+        int count = 0;
+
+        for(int i = 0; i<text.length(); i++) {
+            for (char vowel : vowels) {
+                if (vowel == text.charAt(i)) {
+                    count += 1;
+                    break;
+                }
+            }
+        }
+
+        return count;
     }
 }
